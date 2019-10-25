6b514a6ff7a86a7d9256634e32c794f783ea36db
SheilaBirgen <jeronobergen@gmail.com>
2019-10-25 15:28:20 +0300

    change date formats

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-
-git add
+function weekdays() {
+    var day = document.getElementById("day").value;
+    var enterDate = day.split("-");
+    var dd = parseInt(enterDate[2]);
+    var mm = parseInt(enterDate[1]);
+    var yy = parseInt(enterDate[4]);
