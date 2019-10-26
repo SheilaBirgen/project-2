57ffdc6a89ad94c195a3bac0bdf7a6892c31f8a5
SheilaBirgen <jeronobergen@gmail.com>
2019-10-26 18:01:26 +0300

    add female days of the week

--- js/script.js
+++ js/script.js
@@ -22,3 +22,12 @@ function MakeArray(n) {
  days[5] = "Yaw"
  days[6] = "Kofi"
  
+ //female Initialize Days of Week Array
+ day = new MakeArray(7);
+ day[0] = "Ama"
+ day[1] = "Akosua"
+ day[2] = "Adwoa"
+ day[3] = "Abenaa"
+ day[4] = "Akua"
+ day[5] = "Yaa"
+ day[6] = "Afua"
