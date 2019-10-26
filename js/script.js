c152bdbe9eb78023f8870a39fd375f4a58880f3a
SheilaBirgen <jeronobergen@gmail.com>
2019-10-26 17:59:33 +0300

    add male days of the week

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ function weekdays() {
     var dd = parseInt(enterDate[2]);
     var mm = parseInt(enterDate[1]);
     var yy = parseInt(enterDate[4]);
+    //General Array Function
+function MakeArray(n) {
+    this.length = n;
+    for (var i = 1; i <=n; i++) {
+      this[i] = 0;
+    }
+ }
+ 
+ //male Initialize Days of Week Array
+ days = new MakeArray(7);
+ days[0] = "Kwame"
+ days[1] = "Kwasi"
+ days[2] = "Kwadwo"
+ days[3] = "Kwabena"
+ days[4] = "Kwaku"
+ days[5] = "Yaw"
+ days[6] = "Kofi"
+ 
