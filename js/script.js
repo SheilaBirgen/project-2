a3fde5ea69125c220221abe48d1ea2a34f961f14
SheilaBirgen <jeronobergen@gmail.com>
2019-10-26 18:03:17 +0300

    add months array

--- js/script.js
+++ js/script.js
@@ -31,3 +31,17 @@ function MakeArray(n) {
  day[4] = "Akua"
  day[5] = "Yaa"
  day[6] = "Afua"
+ //Initialize Months Array
+ months = new MakeArray(12);
+ months[1] = "January" 
+ months[2] = "February" 
+ months[3] = "March" 
+ months[4] = "April" 
+ months[5] = "May" 
+ months[6] = "June" 
+ months[7] = "July" 
+ months[8] = "August"
+ months[9] = "September" 
+ months[10] = "October"
+ months[11] = "November"
+ months[12] = "December"
