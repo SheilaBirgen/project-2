94202bf4985389117c20f40804f6ec4b61e5be0a
SheilaBirgen <jeronobergen@gmail.com>
2019-10-28 09:56:02 +0300

    add date fuction

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
 //male Initialize Days of Week Array
- var maleAkanNames=["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
- var femaleAkanNames=["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"]
-var daysofTheWeek=["sunday","monday","teusday","wednesday","thursday","friday","saturday"]
+var maleAkanNames = [
+  "Kwame",
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi"
+];
+var femaleAkanNames = [
+  "Ama",
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua"
+];
+var daysofTheWeek = [
+  "sunday",
+  "monday",
+  "teusday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday"
+];
 
-function compute() {
-   
-  var year=parseInt(document.getElementbyId(yy).value);
-  var mont=parseInt(document.getElementbyId(mm).value);
-  var birthdate=parseInt(document.getElementbyId(dob).value);
+function akanName() {
+  var year = parseInt(document.getElementbyId(yy).value);
+  var month = parseInt(document.getElementbyId(mm).value);
+  var dob = parseInt(document.getElementbyId(dob).value);
 
-  ifdob(dob<=1 || dob>31 )
+  var birthdate=new Date(dob+"/"+mm+"/"+yy+"/").getDay
 
-
-   )
-  var 
+  if (dob <= 1 || dob > 31) {
+    alert("Enter a valid date");
+  } else if (mm <= 1 || mm > 12) {
+    alert("Enter a valid month");
   }
-  
- 
 
- 
+  
+}
