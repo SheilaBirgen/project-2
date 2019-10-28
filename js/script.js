a266d39a65bc936144c57a6dfcb27d330f2c64f0
SheilaBirgen <jeronobergen@gmail.com>
2019-10-28 09:58:10 +0300

    modified script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
-//male Initialize Days of Week Array
-var maleAkanNames = [
-  "Kwame",
-  "Kwasi",
-  "Kwadwo",
-  "Kwabena",
-  "Kwaku",
-  "Yaw",
-  "Kofi"
-];
-var femaleAkanNames = [
-  "Ama",
-  "Akosua",
-  "Adwoa",
-  "Abenaa",
-  "Akua",
-  "Yaa",
-  "Afua"
-];
-var daysofTheWeek = [
-  "sunday",
-  "monday",
-  "teusday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday"
-];
-
 function akanName() {
+  var maleAkanNames = [
+    "Kwame",
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi"
+  ];
+  var femaleAkanNames = [
+    "Ama",
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua"
+  ];
+  var daysofTheWeek = [
+    "sunday",
+    "monday",
+    "teusday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+  ];
+
   var year = parseInt(document.getElementbyId(yy).value);
   var month = parseInt(document.getElementbyId(mm).value);
   var dob = parseInt(document.getElementbyId(dob).value);
 
-  var birthdate=new Date(dob+"/"+mm+"/"+yy+"/").getDay
+  var birthdate = new Date(dob + "/" + mm + "/" + yy + "/").getDay;
 
   if (dob <= 1 || dob > 31) {
     alert("Enter a valid date");
