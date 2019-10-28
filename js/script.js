6b5de48f64564b2c74d200c8ee5f34aa1785c20c
SheilaBirgen <jeronobergen@gmail.com>
2019-10-28 12:51:00 +0300

    change submit button

--- js/script.js
+++ js/script.js
@@ -1,47 +1,45 @@
+var maleAkanNames = [
+  "Kwame",
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi"
+];
+var femaleAkanNames = [
+  "Ama",
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua"
+];
+var daysofTheWeek = [
+  "Sunday",
+  "Monday",
+  "Teusday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 function akanName() {
-  var maleAkanNames = [
-    "Kwame",
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi"
-  ];
-  var femaleAkanNames = [
-    "Ama",
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua"
-  ];
-  var daysofTheWeek = [
-    "Sunday",
-    "Monday",
-    "Teusday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-
-  var yy = parseInt(document.getElementbyId("yy").value);
-  var mm = parseInt(document.getElementbyId("mm").value);
-  var dd = parseInt(document.getElementbyId("dd").value);
+  var yy = parseInt(document.getElementById("yy").value);
+  var mm = parseInt(document.getElementById("mm").value);
+  var dd = parseInt(document.getElementById("dd").value);
   var male = document.getElementById("male");
   var female = document.getElementById("female");
 
-  if (dd < 1 || dd > 31) {
+  if (dd <1 || dd > 31) {
     alert("Enter a valid date");
   } else if (mm < 1 || mm > 12) {
     alert("Enter a valid month");
-  } else yy <= 1500 || yy > 2019;
+  }
 
-  var birthdate = new Date(dd + "/" + mm + "/" + yy);
-  var day = birthdate.getDay();
-  console.log(day);
+  var birthDate = new Date(dd + "/" + mm + "/" + yy);
+  var day = birthDate.getDay();
   if (male.checked == true) {
     alert(
       "you were born on" +
@@ -56,7 +54,5 @@ function akanName() {
         "and your Akan name is" +
         femaleAkanNames[day]
     );
-  } else {
-    alert("An error occured");
   }
 }
